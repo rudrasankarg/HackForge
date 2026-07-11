3a27e4ae70efc73908d2d4c8547479ec135527da
Mystic Maverick <rudrasankarg@gmail.com>
2026-07-12 02:52:01 +0530

    feat: Implement Feature 5 - Jitsi Meet / WebRTC Team Video Rooms

---
+++ frontend/src/pages/participant/VideoHuddle.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function VideoHuddle() {
+  return (
+    <div className="card">
+      <h3>Jitsi Meet Huddle Room</h3>
+      <iframe src="https://meet.jit.si/hackforge-team-huddle" width="100%" height="400" />
+    </div>
+  );
+}
