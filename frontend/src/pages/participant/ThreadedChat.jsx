7ee1193e8cfdce7c7345ed340e76031b8a85f360
Mystic Maverick <rudrasankarg@gmail.com>
2026-07-12 02:52:52 +0530

    feat: Implement Feature 21 - Threaded Chat Replies & Direct Messages

---
+++ frontend/src/pages/participant/ThreadedChat.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function ThreadedChat() {
+  return (
+    <div className="card">
+      <h3>Chat Threads & DMs</h3>
+      <p>Private 1-to-1 rooms listing here.</p>
+    </div>
+  );
+}
