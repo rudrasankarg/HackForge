01d45dabbcd94f2ae19db212f6b01a8b4278bd12
Mystic Maverick <rudrasankarg@gmail.com>
2026-07-12 02:54:21 +0530

    Feature change

--- implement_and_commit.js
+++ implement_and_commit.js
@@ -201,7 +201,7 @@ export default function NotificationsCenter() {
   return (
     <div className="card">
       <h3>Notifications Center</h3>
-      <p>🔔 Your project proposal has been approved by the reviewer.</p>
+      <p> Your project proposal has been approved by the reviewer.</p>
     </div>
   );
 }`
